8c85a1a705b16b0adc9446073d2cca09c3bfa23a
jiangwei1102 <623926420@qq.com>
2018-07-03 12:02:56 +0800

    添加了标签名称的models类

--- artapp/models.py
+++ artapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+# 索引可以提高查找速度,适用于sql优化
+
+
+class ArtTag(models.Model):
+    name = models.CharField(max_length=20, verbose_name='作品类别', unique=True, blank=True, db_index=True)
+    add_time = models.DateTimeField(verbose_name='添加时间', auto_now_add=True)
+    modify_time = models.DateTimeField(verbose_name='最后修改时间', auto_now=True)
+    
+
+
+
