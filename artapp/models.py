0731e6fe3fb9f008c9401d3277b439544b588867
jiangwei1102 <623926420@qq.com>
2018-07-03 14:18:12 +0800

    修改添加了tag模型类

--- artapp/models.py
+++ artapp/models.py
@@ -8,7 +8,7 @@ class ArtTag(models.Model):
     name = models.CharField(max_length=20, verbose_name='作品类别', unique=True, blank=True, db_index=True)
     add_time = models.DateTimeField(verbose_name='添加时间', auto_now_add=True)
     modify_time = models.DateTimeField(verbose_name='最后修改时间', auto_now=True)
-    
+
 
 
 
